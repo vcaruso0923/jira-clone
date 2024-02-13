fd64185b67daa65be4a2576ed2a3611761df5fc2
Vincent Caruso <vinnycar0923@gmail.com>
2024-02-13 15:20:34 -0800

    initial-commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import {Sample} from './components/Sample'
+
+export const App = () => {
+    return (
+        <div className='App'>
+            <body>
+                <p>TypeScript MERN Template</p>
+                <Sample />
+            </body>
+        </div>
+    )
+}
